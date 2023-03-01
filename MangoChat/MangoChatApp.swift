6395b8c9baec9313780068e0ce0d1ed6a066f92e
KangMinGyo <kang15567@kakao.com>
2023-03-01 15:22:25 +0900

    First Commit

---
+++ MangoChat/MangoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangoChatApp.swift
+//  MangoChat
+//
+//  Created by KangMingyo on 2023/03/01.
+//
+
+import SwiftUI
+
+@main
+struct MangoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
